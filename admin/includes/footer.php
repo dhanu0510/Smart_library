9e303ea6975646bd20da55adce8f31d88a77bb93
Dhananjay <180770107549@socet.edu.in>
2020-10-04 15:41:52 +0530

    none

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+<section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Online Library Management System |<a href="#" target="_blank"  > Designed by : Our Design Engineering Team</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
